e06b91af4fe3c19653dd1a90fd8b6792bfaaff32
justbobinaround <roberthenryjunkins@gmail.com>
2023-07-08 00:50:13 -0400

    modified:   index.html 	modified:   src/lib.rs 	modified:   wasm/.gitignore 	modified:   wasm/wasm_ligma_interpreter_bg.wasm

--- src/lib.rs
+++ src/lib.rs
@@ -38,11 +38,11 @@ pub fn ligma(input: String) -> String{
     }
     let mut fun = Fun::new(tokens);
     fun.eval();
-    let output: String;
+    let mut output = String::from("L I G M A  Interprets Generally Meaningless Abstractions\nv0.1.0\n\n");
     let printer = fun.get_output();
     {
         let printer = printer.lock().unwrap();
-        output = printer.clone();
+        output.push_str(&printer);
     }
 
     return output;
